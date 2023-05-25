1634eae14ab0fff3946b808f631561881324812a
wyx <1074333851@qq.com>
2023-05-25 16:21:36 +0800

    第二次提交

--- chat-server/src/main/java/com/ceam/eureka/utils/ConflictTest.java
+++ chat-server/src/main/java/com/ceam/eureka/utils/ConflictTest.java
@@ -91,11 +91,10 @@ public class ConflictTest {
    */
   public static List<Person> dataPre() {
     List<Person> list = new ArrayList<>();
-    list.add(new Person(1, "张三", 19));
-    list.add(new Person(2, "李四", 18));
     list.add(new Person(3, "王二", 17));
     list.add(new Person(4, "麻子", 28));
-    list.add(new Person(5, "李四", 15));
+    list.add(new Person(10, "王五", 29));
+    list.add(new Person(12, "二狗", 23));
     return list;
   }
 }
